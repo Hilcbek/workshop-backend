e38a465528769c5fd0313d8a541775ef39e84f67
Hilcbek <balemayehu07@gmail.com>
2025-07-07 14:44:38 +0300

    Added

--- dist/config/rateLimit.js
+++ dist/config/rateLimit.js
@@ -1,15 +1,11 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.rateLimiter = void 0;
 var _rateLimiterFlexible = require("rate-limiter-flexible");
-var _ioredis = _interopRequireDefault(require("ioredis"));
-var redisClient = new _ioredis["default"]();
 var rateLimiter = exports.rateLimiter = new _rateLimiterFlexible.RateLimiterRedis({
-  storeClient: redisClient,
   keyPrefix: 'middleware',
   points: 100,
   duration: 100,
--- src/config/rateLimit.js
+++ src/config/rateLimit.js
@@ -1,10 +1,6 @@
 import { RateLimiterRedis } from 'rate-limiter-flexible';
-import Redis from 'ioredis';
-
-const redisClient = new Redis(); 
 
 const rateLimiter = new RateLimiterRedis({
-  storeClient: redisClient,
   keyPrefix: 'middleware',
   points: 100,        
   duration: 100,      
