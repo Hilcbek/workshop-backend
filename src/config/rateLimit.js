ba121ffe41be7e01254748312f6617280003a007
Hilcbek <balemayehu07@gmail.com>
2025-07-07 14:51:53 +0300

    added

--- src/config/rateLimit.js
+++ src/config/rateLimit.js
@@ -1,6 +1,16 @@
 import { RateLimiterRedis } from 'rate-limiter-flexible';
+import Redis from 'ioredis';
+import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } from './enviromental';
+
+const redisClient = new Redis({
+  host: REDIS_HOST,
+  port: Number(REDIS_PORT),
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+});
 
 const rateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
   keyPrefix: 'middleware',
   points: 100,        
   duration: 100,      
